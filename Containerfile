b42767c41448b74e7b49f111a286c8fa8a4ed2c3
Reuben Lifshay <rlifshay@gmail.com>
2024-07-14 17:14:38 -0700

    chore: add container

---
+++ Containerfile
@@ -0,0 +1,7 @@
+FROM docker.io/library/ubuntu
+
+LABEL org.opencontainers.image.source=https://github.com/computator/update-recent
+
+COPY update_recent.sh /usr/local/bin/
+
+CMD ["update_recent.sh", "/data"]
--- update_recent.sh
+++ update_recent.sh
@@ -8,7 +8,10 @@ process () {
 	local TARGET
 	TARGET="$TARGET/$1"
 	[ -d "$TARGET" ] && find -L "$TARGET" -mindepth 1 \( -xtype l \( -type l -o -mtime +$2 \) -o -type d -empty \) -delete
-	find -H "$CONTENTDIR" -type f -mtime -$2 -printf '%h\0%p\0' | xargs -0 -xn 2 sh -c 'tgt="'"$TARGET"'/${2#$1}"; [ -d "$tgt" ] || mkdir -p "$tgt"; ln -srt "$tgt" "$3" 2> /dev/null; true' -- "$CONTENTDIR"
+	find -H "$CONTENTDIR" -type f -mtime -$2 -printf '%h\0%p\0' \
+		| xargs -0 -xn 2 sh -c \
+			'tgt="'"$TARGET"'/${2#$1}"; [ -d "$tgt" ] || mkdir -p "$tgt"; ln -srt "$tgt" "$3" 2> /dev/null; true' \
+			-- "$CONTENTDIR"
 }
 
 process 00_day 1
